f202f4a5acb5b0a56174a70fd33b76312fe0a814
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2023-12-06 11:10:18 -0600

    Initial Commit

---
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "MultiplayerSessionsSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERSESSIONS_API UMultiplayerSessionsSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+	UMultiplayerSessionsSubsystem(); 
+
+	//
+	// To handle session functionality. The Menu class will call these.
+	//
+	void CreateSession(int32 NumPublicConnections, FString MatchType);
+	void FindSessions(int32 MaxSearchResults);
+	void JoinSession(const FOnlineSessionSearchResult& SessionResult);
+	void DestroySession();
+	void StartSession();
+	
+protected:
+
+	//
+	// Internal callbacks for the delegates we'll add to the Online Session Interface delegate list.
+	// These don't need to be called outside of this class.
+	//
+	
+private:
+	IOnlineSessionPtr SessionInterface;
+
+	//
+	// To add to the Online Session Interface list.
+	// We'll bind our MultiplayerSessionsSubsystem internal callbacks to these.
+	//
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FOnFindSessionsCompleteDelegate FindSessionsCompleteDelegate;
+	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+	FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;
+	FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+};
